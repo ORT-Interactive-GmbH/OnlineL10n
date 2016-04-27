812a768f3d4ef4c1ed9aa004ac07b8cdbe123f90
Sebastian Westemeyer <sebastian.westemeyer@ort-online.net>
2016-04-27 15:25:39 +0200

    Initial Commit

---
+++ SwiftLocalization/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLocalization
+//
+//  Created by Sebastian Westemeyer on 27.04.16.
+//  Copyright © 2016 ORT Interactive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
